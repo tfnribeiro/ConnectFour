11b04900c0c962e0b6e7029c42e48752da036ea8
Tiago Ribeiro <tfnribeiro@gmail.com>
2021-04-22 19:54:31 +0200

    Starting Player Alternates
    Added logic to alternate players and added controls to avoid command line crashing if not provided with a correct col number (int).

--- Program.cs
+++ Program.cs
@@ -10,88 +10,89 @@ namespace ConnectFour
     {
         static void Main(string[] args)
         {
-            /*
-            State board = new State(6, 7, 1);
-            AiAlphaBeta botP1 = new AiAlphaBeta();
-            AiAlphaBeta botP2 = new AiAlphaBeta();
-            botP1.StartGame(1, 6, 7, 4);
-            botP2.StartGame(2, 6, 7, 2);*/
-            /*
-            while(board.GameIsFinished() == -1)
-            {
-                Console.WriteLine("PLAYER: " + board.GetTurn());
-                board.PrintBoard();
-                int play = int.Parse(Console.ReadLine());
-                board.InsertCoin(play,board.GetTurn());
-                Console.WriteLine("Play Heuristic: " + Heuristic.TokenInRow(board, play));
-                
-            }
-            Console.WriteLine("VICTORY FOR PLAYER: " + board.GetWinner());
-            */
             int tries = 0;
-            int p1V = 0;
-            int p2V = 0;
+            int humanV = 0;
+            int botV = 0;
             int draws = 0;
             int cols = 7;
             int rows = 6;
-            int depth = 9;
+            int depth = 6;
             while (tries < 5)
             {
-
+                Console.Clear();
                 State board = new State(rows, cols, 1);
-                AiAlphaBeta botP1 = new AiAlphaBeta();
-                //AiAlphaBeta botP2 = new AiAlphaBeta();
-                botP1.StartGame(1, rows, cols, depth);
-                //botP2.StartGame(2, rows, cols, depth);
-            
+                AiAlphaBeta bot = new AiAlphaBeta();
+                int botTurn = (tries % 2 == 0) ?  2 : 1;
+                int playerTurn = botTurn == 1 ? 2 : 1;
+                bot.StartGame(botTurn, rows, cols, depth);
+
                 while (board.GameIsFinished() == -1)
                 {
                     Console.Clear();
-                    Console.WriteLine($"Computer Piece | X |");
-                    Console.WriteLine($"Player Piece | O |");
+                    if(botTurn == 1)
+                    {
+                        Console.WriteLine("Computer Piece | X |");
+                        Console.WriteLine("Player Piece | O |");
+                        ComputerMove(bot, botTurn, board);
+                        PlayerMove(bot, playerTurn, board);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Player Piece | X |");
+                        Console.WriteLine("Computer Piece | O |");
+                        PlayerMove(bot, playerTurn, board);
+                        ComputerMove(bot, botTurn, board);
+                        
+                    }
                     Console.WriteLine();
-                    int statesSeen = State.STATES_CREATED;
-                    DateTime calcStart = DateTime.Now;
-                    int play1 = botP1.CalculateNextMove();
-                    botP1.InsertCoin(play1, 1);
-                    //botP2.InsertCoin(play1, 1);
-                    board.InsertCoin(play1, 1);
-                    Console.WriteLine($"COMPUTER MOVE: C{play1+1}");
-                    Console.WriteLine($"States looked at: {State.STATES_CREATED - statesSeen}");
-                    Console.WriteLine($"Time taken: {DateTime.Now.Subtract(calcStart).TotalSeconds} s");
-                    Console.WriteLine(board.PrintBoard());
-                    if (board.GameIsFinished() != -1) break;
-                    Console.Write($"PLAYER MOVE C({1} - {cols}): ");
-                    int play2 = PlayerMove();
-                    botP1.InsertCoin(play2, 2);
-                    //botP2.InsertCoin(play2, 2);
-                    board.InsertCoin(play2, 2);
-                    Console.Clear();
+                    
                 }
-                switch (board.GetWinner())
-                {
-                    case 1: p1V++;
-                        break;
-                    case 2: p2V++;
-                        break;
-                    default: draws++;
-                        break;
-                }
+                if (board.GetWinner() == playerTurn) humanV++;
+                else if (board.GetWinner() == botTurn) botV++;
+                else draws++;
                 tries++;
                 Console.WriteLine(board.PrintBoard());
                 Console.WriteLine("GAME " + tries + " CONCLUDED!");
+                Console.WriteLine($"Human ({humanV}) VS Bot ({botV}), Draws: {draws}");
                 Console.WriteLine("Press any key to continue...");
                 Console.ReadKey();
+                Console.Clear();
             }
-            Console.WriteLine("P1: " + p1V + "\nP2: " + p2V + "\nDraws:" + draws);
+            
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
            
 
         } 
-        public static int PlayerMove()
+        private static void ComputerMove(AiAlphaBeta ai, int playerID, State board)
         {
-            return int.Parse(Console.ReadLine())-1;
+            int statesSeen = State.STATES_CREATED;
+            DateTime calcStart = DateTime.Now;
+            int play1 = ai.CalculateNextMove();
+            ai.InsertCoin(play1, playerID);
+            board.InsertCoin(play1, playerID);
+            Console.WriteLine($"COMPUTER MOVE: C{play1 + playerID}");
+            Console.WriteLine($"States looked at: {State.STATES_CREATED - statesSeen}");
+            Console.WriteLine($"Time taken: {DateTime.Now.Subtract(calcStart).TotalSeconds} s");
+            Console.WriteLine(board.PrintBoard());
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+        }
+        private static void PlayerMove(AiAlphaBeta ai, int playerID, State board)
+        {
+            Console.WriteLine(board.PrintBoard());
+            Console.Write($"PLAYER MOVE C({1} - {board.GetCols()}): ");
+            int move = -1;
+            bool isInt = true;
+            do
+            {
+                if (isInt == false) Console.WriteLine("Invalid Col!");
+                isInt = int.TryParse(Console.ReadLine(), out move);
+            } while ((move < 0 || move > board.GetCols()) || !isInt );
+            ai.InsertCoin(move-1, playerID);
+            board.InsertCoin(move-1, playerID);
+            Console.Clear();
         }
     }
 }
